c5ff3fc421fd9f653f7c62435c24c6f55874a1cc
Weriksousa_ <werikvsousa@gmail.com>
2025-04-20 22:08:32 -0300

    db ativado

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from database import db
+from app.database import db
 
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
--- app/routers/usuarios.py
+++ app/routers/usuarios.py
@@ -1,7 +1,34 @@
 from flask import Blueprint, render_template
+from app.database import db
+from app.models import Usuario
+from flask import request, redirect, flash
 
 bp_usuarios = Blueprint('usuarios', __name__)
 
 @bp_usuarios.route('/create')
 def create():
-    return render_template('usuarios_create.html')
+
+    if request.method == 'GET':
+        return render_template('usuarios_create.html')
+
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+        csenha = request.form.get('csenha')
+
+        u = Usuario(nome, email, senha)
+        db.session.add(u)
+        db.session.commit()
+        if not nome or not email or not senha:
+            flash('Preencha todos os campos')
+            return redirect('/usuarios/create')
+        if senha != csenha:
+            flash('As senhas não conferem')
+            return redirect('/usuarios/create')
+        return 'Usuário criado com sucesso!'
+    
+@bp_usuarios.route('/list')
+def list():
+    usuarios = Usuario.query.all()
+    return render_template('usuarios_list.html', usuarios=usuarios)
